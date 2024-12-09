c661e745d3c0847ad46e3fec4e84ba064ae96b1e
wendolyn <wendolynpedraza@gmail.com>
2024-12-09 12:52:39 -0600

    Cadena de conexion

---
+++ Capa acceso datos/configuracion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_acceso_datos
+{
+    public class configuracion
+
+
+    {
+        //Cadena de conexion 
+        //data sourse=nombre del servidor del DB
+        //localhost
+        //.
+        // Nombre de mi estancia
+        //Inital Catalogo= nombre de BD
+        //=false (Credenciales de acceso)
+        //se habilitan los campos de:
+        //User=;
+        //password=;
+        static string _CadenaConexion = @"Data Sourse= DESKTOP-GGDL61L; 
+                                                Initial Catalog = Transportes; 
+                                                Integrated Security=true;";
+
+        //Encapsulamiento
+        public static string CadenaConexion
+        {
+            get
+            {
+                return _CadenaConexion;
+            }
+        }
+    }
+}
